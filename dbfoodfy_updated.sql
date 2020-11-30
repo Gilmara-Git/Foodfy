f5b49a926b8558d5a305d90a158c38ccd53a12f2
GILMARA_HP\gilma <gilmarapq@hotmail.com>
2020-11-30 11:58:23 -0500

    feat:updated database

--- dbfoodfy_updated.sql
+++ dbfoodfy_updated.sql
@@ -1,10 +1,16 @@
+DROP DATABASE IF EXISTS dbfoodfy
+
+CREATE DATABASE dbfoodfy
+
+
 CREATE TABLE "recipes" (
   "id" SERIAL PRIMARY KEY,
+  "user_id" int,
   "title" text NOT NULL,
-  "information" text, 
+  "information" text,
   "chef_id" int NOT NULL,
   "steps" text[] NOT NULL,
-  "ingredients" text[] NOT NULL,  
+  "ingredients" text[] NOT NULL,
   "created_at" timestamp NOT NULL DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
 );
@@ -27,23 +33,19 @@ CREATE TABLE "chefs" (
   "file_id" int NOT NULL,
   "created_at" timestamp NOT NULL DEFAULT (now()),
   "updated_at" timestamp DEFAULT (now())
-  
 );
 
-
 CREATE TABLE "users" (
-
-"id" SERIAL PRIMARY KEY,
-"name" text NOT NULL,
-"email" text UNIQUE NOT NULL,
-"password" text NOT NULL,
-"reset_token" text,
-"reset_token_expires" text,
-"is_admin" boolean DEFAULT false,
-"created_at" timestamp NOT NULL DEFAULT (now()),
-"updated_at" timestamp DEFAULT (now())
-)
-
+  "id" SERIAL PRIMARY KEY,
+  "name" text NOT NULL,
+  "email" text UNIQUE NOT NULL,
+  "password" text NOT NULL,
+  "reset_token" text,
+  "reset_token_expires" text,
+  "is_admin" boolean DEFAULT false,
+  "created_at" timestamp NOT NULL DEFAULT (now()),
+  "updated_at" timestamp DEFAULT (now())
+);
 
 ALTER TABLE "chefs" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
 
@@ -52,3 +54,30 @@ ALTER TABLE "recipe_files" ADD FOREIGN KEY ("recipe_id") REFERENCES "recipes" ("
 ALTER TABLE "recipe_files" ADD FOREIGN KEY ("file_id") REFERENCES "files" ("id");
 
 ALTER TABLE "recipes" ADD FOREIGN KEY ("chef_id") REFERENCES "chefs" ("id");
+
+ALTER TABLE "recipes" ADD FOREIGN KEY ("user_id") REFERENCES "users" ("id");
+
+-- procedure to get updated_at
+--CREATE FUNCTION trigger_set_time_stamp() 
+--RETURNS TRIGGER AS 
+--$$ 
+--BEGIN 
+--NEW.updated_at = NOW(); 
+--RETURN NEW; 
+--END; 
+--$$ 
+--LANGUAGE plpgsql;
+
+
+--CREATE TRIGGER set_timestamp 
+--BEFORE UPDATE ON recipes 
+--FOR EACH ROW EXECUTE PROCEDURE trigger_set_time_stamp();
+
+ 
+--CREATE TRIGGER set_timestamp 
+--BEFORE UPDATE ON chefs 
+--FOR EACH ROW EXECUTE PROCEDURE trigger_set_time_stamp();
+
+
+
+
